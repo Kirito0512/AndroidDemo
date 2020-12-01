83f6b584dfaabc913777155a1d1f9d3a8ec15fbb
xuqi <xuqi@changba.com>
2020-12-01 10:27:10 +0800

    feat 添加跳过按钮

--- app/src/main/java/com/example/androiddemo/viewhint/AndroidViewHintHelper.java
+++ app/src/main/java/com/example/androiddemo/viewhint/AndroidViewHintHelper.java
@@ -1,20 +1,30 @@
 package com.example.androiddemo.viewhint;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.util.SparseArray;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.IdRes;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+
+import com.example.androiddemo.R;
+
+import org.w3c.dom.Text;
 
 public class AndroidViewHintHelper {
     private final KtvHintView hintView;
     private SparseArray<ViewHintInfo> sparseArray = new SparseArray<>();
     private FragmentActivity activity;
     private View externalView;
+    private TextView skipTv;
+
     public AndroidViewHintHelper(Fragment fragment) {
         this(fragment.getActivity());
     }
@@ -47,6 +57,27 @@ public class AndroidViewHintHelper {
         return this;
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public AndroidViewHintHelper addSkipView() {
+        skipTv = new TextView(activity);
+        skipTv.setText("跳过");
+        skipTv.setTextColor(Color.parseColor("#FF121212"));
+        skipTv.setTextSize(12);
+        skipTv.setGravity(Gravity.CENTER);
+        skipTv.setBackground(skipTv.getResources().getDrawable(R.drawable.skip_textview_background, null));
+        skipTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hintView != null && hintView.getParent() != null) {
+                    FrameLayout frameLayout = (FrameLayout) hintView.getParent();
+                    frameLayout.removeView(hintView);
+                    frameLayout.removeView(skipTv);
+                }
+            }
+        });
+        return this;
+    }
+
     public void show() {
         if (activity == null)
             return;
@@ -57,6 +88,13 @@ public class AndroidViewHintHelper {
         decorView.addView(hintView);
         if (externalView != null) {
             decorView.addView(externalView);
+        }
+        if (skipTv != null) {
+            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(144, 72);
+            lp.gravity = Gravity.BOTTOM | Gravity.START;
+            lp.leftMargin = 45;
+            lp.bottomMargin = 45;
+            decorView.addView(skipTv, lp);
         }
     }
 
